208a48fff8b846c7252275e1b4e8b9fd30a9cc78
Mukul Singh <135323609+Mukul1598@users.noreply.github.com>
2024-08-01 14:15:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstWord(s) {
   // your code here
+	const trimStr = s.trim();
+	const words =trimStr.split(" ");
+	console.log(words);
+	return words[0];
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
